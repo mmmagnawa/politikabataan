b8a3cfe3d6e7d625b24303c0fb2a3dd4b03257a8
ObsidianHydra <33882984+ObsidianHydra@users.noreply.github.com>
2020-10-19 04:24:10 +0800

    fixed script, might add more changes later

--- scripts/transitions.js
+++ scripts/transitions.js
@@ -16,18 +16,24 @@ window.addEventListener('scroll', function(e) {
 	}
 });
 
+//FOR CONTACTS
 
-//for contacts
+//initializing variables
 
 var body = document.getElementById("opcontainer");
 var body2 = document.getElementById("opcontainer2");
-
 document.getElementById("1").addEventListener("click", displayBio0);
+document.getElementById("1_full").addEventListener("click", exitBio0);
 document.getElementById("2").addEventListener("click", displayBio1);
+document.getElementById("2_full").addEventListener("click", exitBio1);
 document.getElementById("3").addEventListener("click", displayBio2);
+document.getElementById("3_full").addEventListener("click", exitBio2);
 document.getElementById("4").addEventListener("click", displayBio3);
+document.getElementById("4_full").addEventListener("click", exitBio3);
 document.getElementById("5").addEventListener("click", displayBio4);
+document.getElementById("5_full").addEventListener("click", exitBio4);
 document.getElementById("6").addEventListener("click", displayBio5);
+document.getElementById("6_full").addEventListener("click", exitBio5);
 
 var profiles = [];
 var clicked = [];
@@ -35,72 +41,126 @@ var other = false;
 for(i=1;i<7;i++){
 	profiles.push(document.getElementById(i.toString() + "_full"));
 	clicked.push(false);
-};
+}
 
 function displayBio0(){
-	if(clicked[0] == false){
+	if(clicked[0] == false && other == false){
 		clicked[0] = true;
+		other = true;
+		body.classList.add("turnOpaque");
+		body2.classList.add("turnOpaque");
 		profiles[0].classList.add("displayProfile_full");
 	}
-	else{
+}
+
+function exitBio0(){
+	if(clicked[0] == true){
 		clicked[0] = false;
+		other = false;
 		profiles[0].classList.remove("displayProfile_full");
+		body.classList.remove("turnOpaque");
+		body2.classList.remove("turnOpaque");
 	}
 }
 
 function displayBio1(){
-	if(clicked[1] == false){
+	if(clicked[1] == false && other == false){
 		clicked[1] = true;
+		other = true;
+		body.classList.add("turnOpaque");
+		body2.classList.add("turnOpaque");
 		profiles[1].classList.add("displayProfile_full");
 	}
-	else{
+}
+
+function exitBio1(){
+	if(clicked[1] == true){
 		clicked[1] = false;
+		other = false;
 		profiles[1].classList.remove("displayProfile_full");
+		body.classList.remove("turnOpaque");
+		body2.classList.remove("turnOpaque");
 	}
 }
 
-function displayBio2(){
-	if(clicked[2] == false){
+;function displayBio2(){
+	if(clicked[2] == false && other == false){
 		clicked[2] = true;
+		other = true;
+		body.classList.add("turnOpaque");
+		body2.classList.add("turnOpaque");
 		profiles[2].classList.add("displayProfile_full");
 	}
-	else{
+}
+
+function exitBio2(){
+	if(clicked[2] == true){
 		clicked[2] = false;
+		other = false;
 		profiles[2].classList.remove("displayProfile_full");
+		body.classList.remove("turnOpaque");
+		body2.classList.remove("turnOpaque");
 	}
 }
+
 
 function displayBio3(){
-	if(clicked[3] == false){
+	if(clicked[3] == false && other == false){
 		clicked[3] = true;
+		other = true;
+		body.classList.add("turnOpaque");
+		body2.classList.add("turnOpaque");
 		profiles[3].classList.add("displayProfile_full");
-	}
-	else{
-		clicked[3] = false;
-		profiles[3].classList.remove("displayProfile_full");
 	}
 }
 
+function exitBio3(){
+	if(clicked[3] == true){
+		clicked[3] = false;
+		other = false;
+		profiles[3].classList.remove("displayProfile_full");
+		body.classList.remove("turnOpaque");
+		body2.classList.remove("turnOpaque");
+	}
+}
+
+
 function displayBio4(){
-	if(clicked[4] == false){
+	if(clicked[4] == false && other == false){
 		clicked[4] = true;
+		other = true;
+		body.classList.add("turnOpaque");
+		body2.classList.add("turnOpaque");
 		profiles[4].classList.add("displayProfile_full");
 	}
-	else{
+}
+
+function exitBio4(){
+	if(clicked[4] == true){
 		clicked[4] = false;
+		other = false;
 		profiles[4].classList.remove("displayProfile_full");
+		body.classList.remove("turnOpaque");
+		body2.classList.remove("turnOpaque");
 	}
 }
 
 function displayBio5(){
-	if(clicked[5] == false){
+	if(clicked[5] == false && other == false){
 		clicked[5] = true;
-		profiles[5].classList.add("displayProfile_full");
+		other = true;
 		body.classList.add("turnOpaque");
 		body2.classList.add("turnOpaque");
+		profiles[5].classList.add("displayProfile_full");
 	}
-	else{
+}
+
+function exitBio5(){
+	if(clicked[5] == true){
 		clicked[5] = false;
+		other = false;
 		profiles[5].classList.remove("displayProfile_full");
+		body.classList.remove("turnOpaque");
+		body2.classList.remove("turnOpaque");
 	}
 }
